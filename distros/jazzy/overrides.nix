5f252f35dfd02d07fd0b417f178faf43248ea55e
MaxKiv <maxkivits42@gmail.com>
2025-07-21 15:00:14 +0200

    fix(canopen): update canopen hash

--- distros/jazzy/overrides.nix
+++ distros/jazzy/overrides.nix
@@ -184,7 +184,7 @@ in {
     url = "https://gitlab.com/lely_industries/lely-core.git";
     rev = "b63a0b6f79d3ea91dc221724b42dae49894449fc";
     fetchgitArgs = {
-      hash = "sha256-x9JCU2Ryssq424n90IzVOxixnvsoYTukyCOL3zNbwt4=";
+      hash = "sha256-q3dSlELhIrQesxoBdHElR2rey29KDNcGjlAbtIVqil0=";
       leaveDotGit = true;
     };
   };
